3c8052156c59834f8014866ed938c83b7c2d3d55
GEera-Batz <geralexbatz@gmail.com>
2019-02-17 22:06:34 -0600

    Este es el primer cambio en la practica 1

---
+++ practicaCorta1/Clases/InicioJuegoyAhorcado.java
@@ -0,0 +1,170 @@
+import java.util.Scanner;
+public class InicioJuegoyAhorcado {
+
+    /**
+     * @param args the command line arguments
+     */
+    static Scanner scanner = new Scanner(System.in);
+    static int opciones;
+    public static void main(String[] args) {        // Verifica si envian un parametro desde el inicio
+                                                                                                            
+        if (args.length < 1) {
+            System.out.println("Bienvenido a la librería de Juegos");
+            seleccionDeJuegos();  
+        }else{
+            parametroEnviado(args[0]);
+        }
+
+    }
+    private static void parametroEnviado(String juego){ //Recibe el parametro enviado y redirige el programa ha donde es solicitado
+        switch (juego) {
+            case "Ahorcado":
+                controlJuegoAhorcado();
+                break;
+            case "Basquetball":
+                JuegoBasquetball.controlJuegoBasquetball();
+                break;
+            case "Cartas":
+                JuegoCartas.juegoCartas();
+                break;
+            case "Hanoi":
+                JuegoHanoi.hanoi();
+                break;
+           
+        }
+    }
+    
+    private static void controlJuegoAhorcado(){
+        
+        System.out.println("Juego del Ahorcado");
+        System.out.println("Que quieres hacer?");
+        System.out.println("1. Jugar");
+        System.out.println("2. Instrucciones");
+        System.out.println("3. Menu de Juegos");
+        System.out.println("3. Salir");
+        opciones = scanner.nextInt();
+        switch(opciones){
+            case 1:
+                System.out.println("Ingrese la palabra a ser buscada, solo letras minusculas");
+                String palabraOculta = scanner.next();
+
+                juegoAhorcado(palabraOculta);
+                break;
+            case 2:
+                System.out.println("La idea es encontrar la palabra indicada al inicio,");
+                System.out.println("el juego le indicara si es correcta o no la letra ingresada.");
+                System.out.println("En caso de que sea correcta, la letra sera colocada en su");
+                System.out.println("posición, si no es correcta, se ira dibujando a una persona");
+                System.out.println("con una soga colocada en el cuello, poco a poco.");
+                System.out.println("El juego termina en el momento en que se descubra la palabra");
+                System.out.println("o sea ahorcada la persona, Buena suerte...!!!");
+		controlJuegoAhorcado();
+                break;
+            case 3:
+                seleccionDeJuegos();
+                break;
+            case 4:
+                salirDelJuego();
+                break;
+        
+        }
+    }
+    private static void juegoAhorcado(String palabraEncontrar){ //Recibe la palabra a encontrar
+        char [] letras = palabraEncontrar.toCharArray(); //Convierte la palabra en un arreglo de tipo Char
+        char [] guionesBajos = new char[letras.length];  
+        char encontrandoPalabra;
+        int error = 0;
+        int errorFigura = 0;
+        int verificarPalabra = 0;
+        boolean victoria = false;
+        
+        
+        for (int i = 0; i < letras.length; i++) {       //llena el arreglo con guiones bajos
+            guionesBajos[i] = '_';
+        }
+        
+        System.out.println(guionesBajos);
+        
+        do{
+            System.out.println("Ingrese la letra que usted crea que esta en la palabra");
+            encontrandoPalabra = scanner.next().charAt(0);
+            for (int i = 0; i < letras.length; i++) {       //verifica si la letra que ingresa el usuario esta en la palabra a encontrar
+                if (encontrandoPalabra == letras[i]) {
+                    guionesBajos[i] = encontrandoPalabra;
+                    verificarPalabra = verificarPalabra + 1;
+                }else{
+                    error = error+1;                        //contador para los errores cometidos
+                }               
+            }
+            if (verificarPalabra == letras.length) {        //verifica si la palabra fue encontrada
+                victoria = true;
+                System.out.println("ha ganado");
+            }else{
+                victoria = false;
+                if (error == letras.length) {               //verifica si fallo
+                    errorFigura = errorFigura + 1;
+                    switch(errorFigura){                    //muestra las figuras del Ahorcado
+                    case 1:
+                        FigurasAhorcados.error1();
+                        break;
+                    case 2:
+                        FigurasAhorcados.error2();
+                        break;
+                    case 3:
+                        FigurasAhorcados.error3();
+                        break;
+                    case 4:
+                        FigurasAhorcados.error4();
+                        break;
+                    case 5:
+                        FigurasAhorcados.error5();
+                        break;
+                    case 6:
+                        FigurasAhorcados.error6();
+                        victoria = true;
+                        System.out.println("Has perdido");
+                        
+                        break;
+                    }
+                }
+            }
+            
+           
+        
+        System.out.println(guionesBajos);
+        error = 0;
+        }while(victoria == false);
+        controlJuegoAhorcado();
+    }
+    public static void salirDelJuego(){         // Finaliza la aplicación
+        System.exit(0);
+    }
+    public static void seleccionDeJuegos(){    //Selecciona el juego que quiere el usuario
+        int opcionJugar;
+        System.out.println("Que juego quieres?");
+        System.out.println("1. Ahorcado");
+        System.out.println("2. Basquetball");
+        System.out.println("3. Juego de Cartas");
+        System.out.println("4. Hanoi");
+        System.out.println("5. No jugar");
+        opcionJugar = scanner.nextInt();
+        switch(opcionJugar){
+            case 1:
+                    controlJuegoAhorcado();
+                break;
+            case 2:
+                    JuegoBasquetball.controlJuegoBasquetball();
+                break;
+            case 3:
+                    JuegoCartas.juegoCartas();
+                break;
+            case 4:
+                    JuegoHanoi.hanoi();
+                break;
+            case 5:
+                    salirDelJuego();
+                break;
+        }
+    } 
+}
+
---
+++ practicaCorta1/Clases/JuegoCartas.java
@@ -0,0 +1,118 @@
+import java.util.Scanner;
+public class JuegoCartas {
+    static Scanner scanner = new Scanner(System.in);
+    public static void juegoCartas() {              //inicio del juego
+        System.out.println("Bienvenido al CASINO 'IPC'");
+        juegoNuevo();
+            
+    }
+    private static void instrucciones() {           //Instrucciones del Juego
+        System.out.println("Instrucciones:");
+        System.out.println("Antes de empezar a jugar, tienes que apostar");
+        System.out.println("Tú indicas cuanto traes de efectivo");
+        System.out.println("El casino estara dando cartas al azar, y");
+        System.out.println("si la carta por la que tu apuestas esta en el ");
+        System.out.println("rango de las que salieron anteriomente, tu ganas");
+        System.out.println("la cantidad que apostaste.");
+        System.out.println("El juego termina hasta que te quedes sin dinero");
+        System.out.println("Buena suerte");
+        juegoNuevo();
+    }
+    private static void juegoNuevo() {              //Pregunta si quiere volver a jugar o hacer otra cosa
+        int jugarDeNuevo;
+        System.out.println("Quieres jugar?");
+        System.out.println("1. Si 2. No 3. Instrucciones 4. Salir de la aplicacion");
+        jugarDeNuevo = Integer.valueOf(scanner.next());
+        switch (jugarDeNuevo) {
+            case 1:
+                controlJuego();
+                break;
+            case 2:
+                System.out.println("Gracias por visitarnos");
+                InicioJuegoyAhorcado.seleccionDeJuegos();
+                break;
+            case 3:  
+                instrucciones();
+                break;
+            case 4:
+                InicioJuegoyAhorcado.salirDelJuego();
+                break;
+        }
+        
+    }
+    private static void controlJuego() {            //Control del juego
+        int efectivo;
+        int primerCarta;
+        int segundaCarta;
+        int tercerCarta;
+        int apuesta;
+        boolean apuestaCorrecta = false;
+        System.out.println("Con cuanto efectivo empezaras a jugar?");   //Recepción del efectivo para jugar
+        efectivo = Integer.valueOf(scanner.next());
+        while (efectivo > 0) {                                          //ciclo para jugar hasta que se le acabe el dinero
+            System.out.println("Efectivo actual: "+efectivo);
+            do {                                                        //ciclo para crear las cartas
+                primerCarta = (int)(Math.random()*11)+1;                //Random hasta 11 porque o puede ser 12 la primer carta
+                segundaCarta = (int)(Math.random()*13)+1;               //Random de 1 a 1
+            } while (primerCarta > segundaCarta);                       //el ciclo termina hasta que la segunda carta sea mayor que la primera
+            
+            tercerCarta = (int)(Math.random()*13)+1;                    //Random para saber que carta es la que sale despues de apostar
+            System.out.println("Las cartas son:");
+            System.out.println("Primer Carta: "+tipoDeCarta(primerCarta)+" Segunda Carta: "+ tipoDeCarta(segundaCarta));
+            do {                                                        //ciclo para saber cuanto apostara el ususario
+                System.out.println("Cuanto Apostaras?");
+                apuesta = Integer.valueOf(scanner.next());
+                if (apuesta > efectivo) {
+                    System.out.println("vuelva a intentarlo");
+                    apuestaCorrecta = false;
+                }else {
+                    apuestaCorrecta = true;
+                }
+            } while (apuestaCorrecta != true);                          //ciclo hasta que laapuesta sea correcta
+            System.out.println("Apostaste: Q"+apuesta);
+            System.out.println("La carta es: "+tipoDeCarta(tercerCarta));
+            if (tercerCarta >= primerCarta && tercerCarta <= segundaCarta) {    //verifica si el usuario aposto bien o no
+                System.out.println("Has ganado");                               
+                efectivo = efectivo + apuesta;                                  //suma lo que fue apostado
+            }else{
+                efectivo = efectivo - apuesta;                                  //resta lo que fue apostado
+                System.out.println("Has apostado mal");
+            }
+        }
+        juegoNuevo();
+    }
+    
+    private static String tipoDeCarta(int carta){  //retorna un string, dependiendo del valor que se le envie por parametros
+        switch (carta) {
+            case 1:
+                return "A";
+            case 2:
+                return "2";
+            case 3:
+                return "3";
+            case 4:
+                return "4";
+            case 5:
+                return "5";
+            case 6:
+                return "6";
+            case 7:
+                return "7";
+            case 8:
+                return "8";
+            case 9:
+                return "9";
+            case 10:
+                return "10";
+            case 11:
+                return "J";
+            case 12:
+                return "Q";
+            case 13:
+                return "K";
+        }
+        return "no valido";
+    }
+    
+}
+
